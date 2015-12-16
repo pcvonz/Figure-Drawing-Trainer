3aeaa4a30522890dec08dbb3728fea07fdf8efad
Paul VonZimmerman <vonzimp@wwu.edu>
2015-12-15 18:55:26 -0800

    Comments and renaming stuff

--- static/script.js
+++ static/script.js
@@ -2,107 +2,88 @@ var n = 1;
 var data;
 var font = 0
 time = new Date().getTime() / 1000;
-var sidebar;
-
-//languages
-var language = "en";
-var currentLanguage = "en";
 
 //Button to grab new article.
-var new_article = false;
 
 function draw() {
-    //If current index is undefined then the end of the article has been reached.
-    //Grab another article.
-    if(window.variable[n] == undefined) {
-            window.cancelAnimationFrame(draw);
-            getJson();
-    }
-    else if(currentLanguage != language) {
-        currentLanguage = language;
-        console.log("lang " + language);
-        console.log("cur " +currentLanguage);
-        getJson();
-    } else if(new_article == true) {
-        new_article = false;
-        getJson();
+    //If there are no more images in the image_list, stop drawing.
+    if (window.image_list[n] == undefined) {
+        window.cancelAnimationFrame(draw);
     } else {
-            var textSpeed = document.getElementById('speed').value;
-            var next_image = document.getElementById('next_image').checked;
-            currentLanguage = language;
-            curr_time = new Date().getTime() / 1000;
-            deltaTime = curr_time - time;
-            var canvas = document.getElementById('Wikipedia');
-            var img = new Image();
-			img.src = window.variable[n]
-            if(canvas.getContext){
-                //Set up canvas
+        //determined by the sidebar speed input
+        var imageTransitionTime = document.getElementById('speed').value;
 
-                var ctx = canvas.getContext('2d');
+        //The checkbox in the sidebar determines this value
+        var next_image = document.getElementById('next_image').checked;
 
-                //get window height
-                var x = canvas.width / 2;
-                var y = canvas.height / 2;
+        //This is used to calculate the time elapsed
+        curr_time = new Date().getTime() / 1000;
+        deltaTime = curr_time - time;
 
+        //Uses an html canvas
+        //mdn: https://developer.mozilla.org/en-US/docs/Web/API/Canvas_API/Tutorial
+        var canvas = document.getElementById('Trainer');
+        var img = new Image();
+        img.src = window.image_list[n]
+        if (canvas.getContext) {
+            //Set up canvas
+            var ctx = canvas.getContext('2d');
 
-                //Render wikipedia text
-                if(deltaTime > textSpeed || next_image == true) {
-					document.getElementById('next_image').checked = false;
-					console.log(deltaTime)
-                    //clear canvas and set it to size of the window
-                    width = $( window ).width();
-                    width = width - (width * .01);
-                    height = $( window ).height();
-                    canvas.width = width;
-                    canvas.height = height;
-                    canvas.style.width = width;
-                    canvas.style.height = height;
+            //get window height
+            var x = canvas.width / 2;
+            var y = canvas.height / 2;
 
-                    //Set the canvas background to black
-                    ctx.fillRect(0, 0, width, height)
+            //Render images
+            if (deltaTime > imageTransitionTime || next_image == true) {
+                document.getElementById('next_image').checked = false;
 
+                //clear canvas and set it to size of the window
+                width = $(window).width();
+                width = width - (width * .01);
+                height = $(window).height();
+                canvas.width = width;
+                canvas.height = height;
+                canvas.style.width = width;
+                canvas.style.height = height;
 
-                    //Calculate appropriate font size
-                    font = ($( window ).width() / 10).toString() + "px sans";
+                //Set the canvas background to black
+                ctx.fillRect(0, 0, width, height)
 
-                    ctx.font = font;
-                    ctx.textAlign = 'center';
-                    ctx.fillStyle = "#e5e5e5"
-                    if (img.height > height) {
-                        img.height = height - 50;
-                    }
-					ctx.drawImage(img, (width / 2) - (img.width / 2), (height / 2) - (img.height / 2));
-                    n++;
-                    time = new Date().getTime() / 1000;
+                //Trying to figure out how to scale the image if the image is too large
+                //To fit on the screen (doesn't work right now)
+                if (img.height > height) {
+                    img.height = height - 50;
+                }
+
+                //Draws the images in the center of the screen
+                ctx.drawImage(img, (width / 2) - (img.width / 2), (height / 2) - (img.height / 2));
+                n++;
+                time = new Date().getTime() / 1000;
             }
             window.requestAnimationFrame(draw);
         }
     }
 }
 
-//Grabs images
+//Ajax request to grab images
 function getJson() {
-    if(language == "en") {
-        var url = 'http://127.0.0.1:5000/grab_article_en';
-    }
-    $.getJSON(url,
-    function(data, textStatus, jqXHR) {
-        window.variable = data;
-        window.sidebar = document.getElementById('sidebar');
-        document.getElementById("article_title").innerHTML= data[0];
-        document.getElementById("link_to_article").href = data[0];
-        draw();
-    }
+    url = document.getElementById('url_form').value
 
-)
+    url = 'http://127.0.0.1:5000/scrape_images?url=' + url;
+    console.log(url);
+    $.getJSON(url,
+        function(data, textStatus, jqXHR) {
+            window.image_list = data;
+            document.getElementById("article_title").innerHTML = data[0];
+            document.getElementById("link_to_article").href = data[0];
+            draw();
+        }
+
+    )
 };
 
-function grabNewArticle() {
-    new_article = true;
+function sidebar() {
+  $('sidebar_button').click(function() {
+    $(this).toggleClass("hover");
+  });
 }
-
-function start() {
-    sidebar = document.getElementById('relative_sidebar');
-    }
-
-window.onload = getJson;
